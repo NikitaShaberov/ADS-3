fb89d4151aa64d4c295c45eee906f988d86fcadf
NikitaShaberov <99290177+NikitaShaberov@users.noreply.github.com>
2022-03-13 22:07:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,30 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int count = 0;
+    int sred;
+    int i = 0, j = size - 1;
+    while (i <= j) {
+      int sredn = (j + i) / 2;
+      if (arr[sredn] == value) {
+         count++;
+         sred = sredn;
+         break;
+        }
+        if (value < arr[sredn])
+          j = sredn - 1;
+        else
+          i = sredn + 1;
+    }
+    if (sred >= 0) {
+      for (int m = 0; m < sred; m++) {
+          if (arr[m] == value)
+              count++;
+        }
+       for (int l = size - 1; l > sred; l--) {
+          if (arr[l] == value)
+              count++;
+        }
+    }
+    return n;
 }
